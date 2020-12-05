17f5d6851132197e164cd78b4dff2e82a8612d42
HandaR <midrau18@gamil.com>
2020-12-05 20:39:31 +0900

    一覧表示機能を実装しました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,19 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    if @product.save
+      redirect_to("/")
+    else
+      render :new
+    end
   end
 
   def edit
