594321249512f6aa0f686e2e3526c3d02f68815d
HandaR <midrau18@gamil.com>
2020-12-07 17:52:02 +0900

    productにcategory_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_074713) do
+ActiveRecord::Schema.define(version: 2020_12_07_083910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,10 @@ ActiveRecord::Schema.define(version: 2020_12_07_074713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "maker_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["maker_id"], name: "index_products_on_maker_id"
   end
 
+  add_foreign_key "products", "categories"
 end
