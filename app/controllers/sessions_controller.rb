363b9dd38dc51c0d2a5ce37db05a562688ce3f83
HandaR <midrau18@gamil.com>
2020-12-12 14:57:40 +0900

    ログイン、ログアウト機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,23 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
 
     if user&.authenticate(session_params[:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       flash[:notice] = "ログインしました"
       redirect_to("/")
     else
-      render :new
+      render("/login")
     end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/")
+  end
+  
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
