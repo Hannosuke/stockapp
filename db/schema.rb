9cc2bf1fd8c08d8576569d3c4ead68a48e03d6f4
HandaR <midrau18@gamil.com>
2020-12-20 14:21:55 +0900

    Purchaseモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_082450) do
+ActiveRecord::Schema.define(version: 2020_12_20_051043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,13 @@ ActiveRecord::Schema.define(version: 2020_12_17_082450) do
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["maker_id"], name: "index_products_on_maker_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "stocks", force: :cascade do |t|
